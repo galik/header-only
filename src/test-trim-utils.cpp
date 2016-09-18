71628331e809538116d0c8edf610009b4067f181
galik <galik@users.noreply.github.com>
2016-09-18 09:34:58 +0100

    test now works with free function trimmers

--- src/test-trim-utils.cpp
+++ src/test-trim-utils.cpp
@@ -28,7 +28,8 @@
 #include <numeric>
 
 #include "test.h"
-#include <hol/experimental/utility_trimmers.h>
+#include <hol/string_utils.h>
+//#include <hol/experimental/utility_trimmers.h>
 
 using namespace hol;
 using namespace std::literals::string_literals;
@@ -58,14 +59,32 @@ std::istream& sgl(std::istream&& is, std::string& line, char delim = '\n')
 	return sgl(is, line, delim);
 }
 
-template<typename Trimmer>
-result_vec generic_trimmer_tests(Trimmer trim, str const& name, std::size_t N = 1)
+struct test_info
 {
-	result_vec results;
+	using vec = std::vector<test_info>;
+	std::size_t line_number;
+	std::string question;
+	std::string answer;
+	std::string result;
+	bool success = false;
+	test_info(std::size_t l, str q, str a)
+	: line_number(l), question(q), answer(a) {}
+};
 
-	std::ifstream ifs;
+struct test_group
+{
+	using vec = std::vector<test_group>;
+	std::string remove;
+	test_info::vec lefts;
+	test_info::vec rights;
+	test_info::vec boths;
+};
 
-	ifs.open("data/test-trim-utils-01.txt");
+test_group::vec load_tests()
+{
+	test_group::vec tests;
+
+	std::ifstream ifs("data/test-trim-utils-01.txt");
 
 	str line;
 	auto id = 0U;
@@ -79,10 +98,13 @@ result_vec generic_trimmer_tests(Trimmer trim, str const& name, std::size_t N = 
 		if((pos = line.find("//")) != str::npos)
 			line.erase(pos);
 
-		if(trim_mutator().round(line).empty())
+		if(hol::trim_mute(line).empty())
 			continue;
 
-		//		config: "{" "}"
+//		if(trim_mutator().round(line).empty())
+//			continue;
+
+		//		S "{}"
 		//		L "{{{}}}" "}}}"
 		//		L "{ {} }" " {} }"
 		//		R "{{{}}}" "{{{"
@@ -90,56 +112,87 @@ result_vec generic_trimmer_tests(Trimmer trim, str const& name, std::size_t N = 
 		//		B "{{{}}}" ""
 		//		B "{ {} }" " {} "
 
-		if(!line.find("config:"))
+		if(!line.find("S"))
 		{
-			str l, r;
-			if(!sgl(sgl(sgl(sgl(std::istringstream(line) >> l, l, '"'), l, '"'), r, '"'), r, '"'))
+			str s;
+			if(!sgl(sgl(std::istringstream(line) >> s, s, '"'), s, '"'))
 				hol_throw_exception(std::runtime_error, "bad format line: " << line_number);
 
-			trim.remove_from_left(l);
-			trim.remove_from_right(r);
+			tests.emplace_back();
+			tests.back().remove = s;
 		}
 		else if(!line.find_first_of("LRB"))
 		{
-
-			str from, to;
-			if(!sgl(sgl(sgl(sgl(std::istringstream(line) >> from, from, '"'), from, '"'), to, '"'), to, '"'))
+			str q, a;
+			if(!sgl(sgl(sgl(sgl(std::istringstream(line) >> q, q, '"'), q, '"'), a, '"'), a, '"'))
 				hol_throw_exception(std::runtime_error, "bad format line: " << line_number);
 
-			result r {name, id++};
-			r.successful();
-			for(decltype(N) i = 0; i < N; ++i)
+			switch(line[0])
 			{
-				auto fr = from;
-				switch(line[0])
-				{
-					case 'L':
-						if(trim.left(fr) != to)
-							r.unsuccessful("Failed test line: "
-								+ std::to_string(line_number) + " " + line
-								+ " '" + std::string(trim.left(fr)) + "'");
+				case 'L':
+					tests.back().lefts.emplace_back(line_number, q, a);
 					break;
-					case 'R':
-						if(trim.right(fr) != to)
-							r.unsuccessful("Failed test line: "
-								+ std::to_string(line_number) + " " + line
-								+ " '" + std::string(trim.right(fr)) + "'");
+				case 'R':
+					tests.back().rights.emplace_back(line_number, q, a);
 					break;
-					case 'B':
-						if(trim.round(fr) != to)
-							r.unsuccessful("Failed test line: "
-								+ std::to_string(line_number) + " " + line
-									+ " '" + std::string(trim.round(fr)) + "'");
+				case 'B':
+					tests.back().boths.emplace_back(line_number, q, a);
 					break;
-				}
+				break;
 			}
-			r.stop();
-
-			results.push_back(r);
 		}
 	}
 
-	return results;
+	return tests;
+}
+
+template<typename TrimmerL, typename TrimmerR, typename TrimmerB>
+void generic_trimmer_tests(
+	str const& name,
+	test_group& test,
+	TrimmerL trim_left,
+	TrimmerR trim_right,
+	TrimmerB trim_both)
+{
+	for(auto& t: test.lefts)
+		if((t.result = trim_left(t.question, test.remove)) == t.answer)
+			t.success = true;
+
+	for(auto& t: test.rights)
+		if((t.result = trim_right(t.question, test.remove)) == t.answer)
+			t.success = true;
+
+	for(auto& t: test.boths)
+		if((t.result = trim_both(t.question, test.remove)) == t.answer)
+			t.success = true;
+}
+
+void display_results(str const& name, test_group::vec const& tests)
+{
+	std::cout << std::boolalpha;
+
+	for(auto const& test: tests)
+	{
+		std::cout << name << ": '" << test.remove << "'" << '\n';
+		for(auto t: test.lefts)
+		{
+			std::cout << "    : L : '" << t.question << "' -> '" << t.answer << "': " << t.success << '\n';
+			if(!t.success)
+				std::cout << "    : E : '" << t.result << "'" << '\n';
+		}
+		for(auto t: test.rights)
+		{
+			std::cout << "    : R : '" << t.question << "' -> '" << t.answer << "': " << t.success << '\n';
+			if(!t.success)
+				std::cout << "    : E : '" << t.result << "'" << '\n';
+		}
+		for(auto t: test.boths)
+		{
+			std::cout << "    : B : '" << t.question << "' -> '" << t.answer << "': " << t.success << '\n';
+			if(!t.success)
+				std::cout << "    : E : '" << t.result << "'" << '\n';
+		}
+	}
 }
 
 int main()
@@ -150,32 +203,27 @@ int main()
 
 	try
 	{
-		std::vector<std::tuple<std::string, float>> all_times;
-		result_vec all_results;
+		auto tests = load_tests();
 
-		result_vec results;
-		results = generic_trimmer_tests(trim_mutator(), "mute", N);
-		all_times.emplace_back("mute", std::accumulate(results.begin(), results.end(), 0.f, [](auto t, auto r){return r.time() + t;}));
-		std::get<float>(all_times.back()) /= results.size();
-		all_results.insert(all_results.end(), results.begin(), results.end());
+		for(auto& test: tests)
+			generic_trimmer_tests("mute", test,
+				[&](str s, str r)->str{return hol::trim_left_mute(s, r.c_str());},
+				[&](str s, str r)->str{return hol::trim_right_mute(s, r.c_str());},
+				[&](str s, str r)->str{return hol::trim_mute(s, r.c_str());}
+				);
 
-		results = generic_trimmer_tests(trim_copier(), "copy", N);
-		all_times.emplace_back("copy", std::accumulate(results.begin(), results.end(), 0.f, [](auto t, auto r){return r.time() + t;}));
-		std::get<float>(all_times.back()) /= results.size();
-		all_results.insert(all_results.end(), results.begin(), results.end());
+		display_results("mute", tests);
 
-		results = generic_trimmer_tests(trim_viewer(), "view", N);
-		all_times.emplace_back("view", std::accumulate(results.begin(), results.end(), 0.f, [](auto t, auto r){return r.time() + t;}));
-		std::get<float>(all_times.back()) /= results.size();
-		all_results.insert(all_results.end(), results.begin(), results.end());
+		tests = load_tests();
 
-		std::stable_sort(all_results.begin(), all_results.end(), [](auto l, auto r){return l.id < r.id;});
+		for(auto& test: tests)
+			generic_trimmer_tests("copy", test,
+				[&](str s, str r)->str{return hol::trim_left_copy(s, r.c_str());},
+				[&](str s, str r)->str{return hol::trim_right_copy(s, r.c_str());},
+				[&](str s, str r)->str{return hol::trim_copy(s, r.c_str());}
+				);
 
-		for(auto const& r: all_results)
-			OUT(r.name << ": " << (r.id<10?"0":"") << r.id << ": " << (r?"P":"F") << " " << r.time() << (r?""s:" " + r.reason + " "));
-
-		for(auto t: all_times)
-			OUT(std::get<0>(t) << ": " << std::get<1>(t));
+		display_results("copy", tests);
 	}
 	catch(const std::exception& e)
 	{
