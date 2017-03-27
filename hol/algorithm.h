da20f864558f5254d6229a4a345d19b743d3f8c8
galik <galik@users.noreply.github.com>
2017-03-27 12:48:33 +0100

    new (untested) algorithms

--- hol/algorithm.h
+++ hol/algorithm.h
@@ -28,45 +28,82 @@
 namespace header_only_library {
 namespace algorithms {
 
-/**
- * Safer erase_remove uses the two iterator erase overload so the
- * caller can't forget and get the single iterator overload by mistake
- * leading to UB.
- *
- * @param c The container to erase from.
- * @param b The first element from which to search for removals.
- * @param e The element beyond the last from which to search for removals.
- * @param v The value to search to be removed.
- */
-template<typename Container>
-void erase_remove(Container& c,
-	typename Container::iterator b,
-	typename Container::iterator e,
-	typename Container::value_type v)
-{
-	c.erase(std::remove(b, e, v), e);
-}
+///**
+// * Safer erase_remove uses the two iterator erase overload so the
+// * caller can't forget and get the single iterator overload by mistake
+// * leading to UB.
+// *
+// * @param c The container to erase from.
+// * @param b The first element from which to search for removals.
+// * @param e The element beyond the last from which to search for removals.
+// * @param v The value to search to be removed.
+// */
+//template<typename Container>
+//void erase_remove(Container& c,
+//	typename Container::iterator b,
+//	typename Container::iterator e,
+//	typename Container::value_type v)
+//{
+//	c.erase(std::remove(b, e, v), e);
+//}
+//
+//template<typename Container>
+//void erase_remove(Container& c,
+//	typename Container::value_type v)
+//{
+//	erase_remove(c, std::begin(c), std::end(c), v);
+//}
+//
+//template<typename Container, typename Pred>
+//void erase_remove_if(Container& c,
+//	typename Container::iterator b,
+//	typename Container::iterator e,
+//	Pred p)
+//{
+//	c.erase(std::remove_if(b, e, p), e);
+//}
+//
+//template<typename Container, typename Pred>
+//void erase_remove_if(Container& c, Pred p)
+//{
+//	erase_remove_if(c, std::begin(c), std::end(c), p);
+//}
 
-template<typename Container>
-void erase_remove(Container& c,
-	typename Container::value_type v)
+template<typename Container, typename Value>
+auto find_in(Container&& c, Value const& v)
 {
-	erase_remove(c, std::begin(c), std::end(c), v);
+	return std::find(std::begin(std::forward<Container>(c)), std::end(std::forward<Container>(c)), v);
 }
 
 template<typename Container, typename Pred>
-void erase_remove_if(Container& c,
-	typename Container::iterator b,
-	typename Container::iterator e,
-	Pred p)
+auto find_in_if(Container&& c, Pred p)
 {
-	c.erase(std::remove_if(b, e, p), e);
+	return std::find_if(std::begin(std::forward<Container>(c)), std::end(std::forward<Container>(c)), p);
+}
+
+template<typename Container, typename Generator>
+void shuffle_all(Container&& c, Generator&& g)
+{
+	std::shuffle(std::begin(std::forward<Container>(c)), std::end(std::forward<Container>(c)), std::forward<Container>(g));
+}
+
+template<typename Container>
+void shuffle_all(Container&& c)
+{
+	thread_local static std::mt19937 mt{std::random_device{}()};
+	shuffle_all(std::forward<Container>(c), mt);
+}
+
+template<typename Container, typename Value>
+void erase_remove_from(Container&& c, Value const& v)
+{
+	c.erase(std::remove(std::begin(std::forward<Container>(c)), std::end(std::forward<Container>(c)), v)), std::end(c));
 }
 
 template<typename Container, typename Pred>
-void erase_remove_if(Container& c, Pred p)
+void erase_remove_from_if(Container&& c, Pred p)
 {
-	erase_remove_if(c, std::begin(c), std::end(c), p);
+	c.erase(std::remove_if(std::begin(std::forward<Container>(c)), std::end(std::forward<Container>(c)), p)), std::end(c));
 }
 
 } // algorithms
