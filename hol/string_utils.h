6d834585f193d284c565f0f5d4722ac176835808
galik <galik@users.noreply.github.com>
2017-06-07 06:50:07 +0100

    tidy up join() algorithm

--- hol/string_utils.h
+++ hol/string_utils.h
@@ -1280,16 +1280,12 @@ template<typename Iter,
 	typename C, typename T = std::char_traits<C>, typename A = std::allocator<C>>
 String<C, T, A> join(Iter begin, Iter end, String<C, T, A> const& delim)
 {
-	auto distance = std::distance(begin, end);
+	auto size = ((std::distance(begin, end) - 1) * delim.size());
 
-	auto size = std::accumulate(begin, end, std::size_t(0),
-	[](auto n, auto const& s)
-	{
-		return n + s.size();
-	}) + ((distance - 1) * delim.size());
+	size += std::accumulate(begin, end, std::size_t(0),
+		[](auto n, auto const& s){ return n + s.size(); });
 
 	String<C, T, A> s;
-
 	s.reserve(size);
 
 	if(begin != end)
